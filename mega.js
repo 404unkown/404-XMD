3d90d299efc8293fe73a9c797895cf99768aaae8
404unkown <francomanucho9@gmail.com>
2025-11-15 22:49:04 +0300

    Replaced old bot with new updated version

---
+++ mega.js
@@ -0,0 +1,62 @@
+import * as mega from 'megajs';
+
+// Mega authentication credentials
+const auth = {
+    email: 'francomanucho9@gmail.com',
+    password: '254matianYi!!',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+};
+
+export const upload = (data, name) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const storage = new mega.Storage(auth, () => {
+                const uploadStream = storage.upload({ name: name, allowUploadBuffering: true });
+
+                data.pipe(uploadStream);
+
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            storage.close();
+                            resolve(url);
+                        }
+                    });
+                });
+
+                storage.on("error", (error) => {
+                    reject(error);
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+export const download = (url) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const file = mega.File.fromURL(url);
+
+            file.loadAttributes((err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                file.downloadBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer);
+                    }
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
