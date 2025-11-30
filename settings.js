f261e9c989f88d2e8fe70ad26bf7c2f86c727007
404unkown <francomanucho9@gmail.com>
2025-11-30 04:15:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.4",
+  version: "1.2",
   updateZipUrl: "https://github.com/404unkown/404-XMD/archive/refs/heads/main.zip",
 };
 
